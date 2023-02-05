80d1cce48f282f12d009b378043d1a9e5d42500e
ArfeenDev <124502205+ArfeenDev@users.noreply.github.com>
2023-02-05 17:46:24 +0530

    Update Whatsapp_auto_messager.py

--- Whatsapp_auto_messager.py
+++ Whatsapp_auto_messager.py
@@ -1,10 +1,13 @@
-import pyautogui as auto
+#before using this code make sure you have installed whatsapp and logged in
+#ArfeenDev
+import pyautogui as auto #importing module
 import time
 
-whatsapp_no= auto.prompt(text='Enter Mobile number here:', title='Whatsapp Messsager:' , default='')
-message=auto.prompt(text='Type message here!', title='Message to be sent on: {}'.format(whatsapp_no), default='')
-confirm=auto.confirm(text='Are sure want confirm', title='Confirm', buttons=['OK', 'Cancel'])
-while confirm=='OK':
+whatsapp_no= auto.prompt(text='Enter Mobile number here:', title='Whatsapp Messsager:' , default='') #getting mobile no 
+#name= auto.prompt(text='Enter Name here:', title='Whatsapp Messsager:' , default='') #If you want to search name and send message
+message=auto.prompt(text='Type message here!', title='Message to be sent on: {}'.format(whatsapp_no), default='') #getting information what to send on whatsapp
+confirm=auto.confirm(text='Are sure want confirm', title='Confirm', buttons=['OK', 'Cancel']) #confirming whether to send or not
+while confirm=='OK': #if we press 'OK' then it will continue
  auto.press('Win', interval=1)
  time.sleep(0.5)
  auto.write('whatsapp')
@@ -13,7 +16,7 @@ while confirm=='OK':
  time.sleep(1)
  auto.click(x=165, y=111)
  time.sleep(1)
- auto.write(whatsapp_no)
+ auto.write(whatsapp_no) # replace with 'name' variable if you want to search name and send message instead of number
  time.sleep(2)
  auto.click(x=196, y=178)
  time.sleep(1)
@@ -25,6 +28,9 @@ while confirm=='OK':
  time.sleep(1)
  break
  
- 
-else:
+ #ArfeenDev
+else: #if we press 'CANCEL' it will be exited and informed in console log
  print("Successfully Exited ✅")
+
+#ArfeenDev
+#ArfeenDev
